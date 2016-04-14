fb4c119594ce376ca8d610f4202d809303661ee3
aarondemarre <ademarre@gmail.com>
2016-04-13 22:01:03 -0700

    Initial commit

---
+++ DRLPTest/DRParserTest.xaml.cs
@@ -0,0 +1,192 @@
+﻿using DRTimeCruncher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DRLPTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class DRParserTest : Window
+    {
+        private Rally rallyData;
+
+        public DRParserTest()
+        {
+            InitializeComponent();
+
+            rallyData = new Rally();
+        }
+
+        private void button_parseStage_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: move all parsing code to an external module
+
+            // get text from textbox
+            var lineCount = textBox_resultsInput.LineCount;
+
+            if (lineCount < 1)
+                return;
+
+            var lines = textBox_resultsInput.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            // parse into stage data
+            var stageData = new Stage();
+
+            foreach (var line in lines)
+            {
+                var splitLine = line.Split('\t');
+                var splitCount = splitLine.Count();
+
+                for (int i = 0; i < splitCount; i++)
+                    splitLine[i] = splitLine[i].Trim();
+
+                if (splitCount == 6)
+                    stageData.AddDriver(new DriverTime(Int32.Parse(splitLine[0]), splitLine[2], splitLine[3], splitLine[4], splitLine[5], splitLine[1]));
+                else if (splitCount == 5)
+                    stageData.AddDriver(new DriverTime(Int32.Parse(splitLine[0]), splitLine[2], splitLine[3], splitLine[4], splitLine[5]));
+                //else
+                    //bad parse fail
+            }
+
+            rallyData.AddStage(stageData);
+
+			textBox_resultsInput.Text = String.Format("SS{0} Has been parsed.", rallyData.StageCount);
+        }
+
+		private void button_crunchNumbers_Click(object sender, RoutedEventArgs e)
+		{
+			rallyData.CalculateTimes();
+			textBox_resultsInput.Text = "Numbers Crunched.";
+			
+		}
+
+		private void button_clearAllData_Click(object sender, RoutedEventArgs e)
+		{
+			rallyData = new Rally();
+			textBox_resultsInput.Text = "Data Cleared.";
+		}
+
+		private void button_printOverallTimes_Click(object sender, RoutedEventArgs e)
+		{
+			var outputSB = new StringBuilder();
+
+			int stageCount = 1;
+			foreach (Stage stage in rallyData)
+			{
+				outputSB.AppendLine("SS" + stageCount);
+				outputSB.AppendLine("Overall");
+				outputSB.AppendLine("Pos, Pos Chng, Name, Vehicle, Time, Diff 1st, Diff Prev");
+
+				List<KeyValuePair<string, DriverTime>> sortedStageData = stage.DriverTimes.ToList();
+				sortedStageData.Sort((x, y) =>
+				{
+					if (x.Value != null && y.Value == null)
+						return -1;
+					else if (x.Value == null && y.Value != null)
+						return 1;
+					else if (x.Value == null && y.Value == null)
+						return 0;
+					else
+						return x.Value.CalculatedOverallTime.CompareTo(y.Value.CalculatedOverallTime);
+				});
+
+				foreach (KeyValuePair<string, DriverTime> driverTimeKvp in sortedStageData)
+				{
+					var driverName = driverTimeKvp.Key;
+					var driverTime = driverTimeKvp.Value;
+
+					if (driverTime != null)
+					{
+						var formatString = @"mm\:ss\.ff";
+						var line = driverTime.OverallPosition + "," +
+								   driverTime.CalculatedPositionChange + "," +
+								   driverTime.Driver + "," +
+								   driverTime.Vehicle + "," +
+								   driverTime.CalculatedOverallTime.ToString(formatString) + "," +
+								   driverTime.CalculatedOverallDiffFirst.ToString(formatString) + "," +
+								   driverTime.CalculatedOverallDiffPrevious.ToString(formatString);
+
+						outputSB.AppendLine(line);
+					}
+					else
+					{
+						outputSB.AppendLine("," + driverName + ",DNF");
+					}
+				}
+
+				outputSB.AppendLine("");
+				stageCount++;
+			}
+
+			textBox_resultsInput.Text = outputSB.ToString();
+		}
+
+		private void button_printStageTimes_Click(object sender, RoutedEventArgs e)
+		{
+			var outputSB = new StringBuilder();
+
+			int stageCount = 1;
+
+			foreach (Stage stage in rallyData)
+			{
+				outputSB.AppendLine("SS" + stageCount);
+				outputSB.AppendLine("Stage");
+				outputSB.AppendLine("Pos, Name, Vehicle, Time, Diff 1st, Diff Prev");
+
+				List<KeyValuePair<string, DriverTime>> sortedStageData = stage.DriverTimes.ToList();
+				sortedStageData.Sort((x, y) =>
+				{
+					if (x.Value != null && y.Value == null)
+						return -1;
+					else if (x.Value == null && y.Value != null)
+						return 1;
+					else if (x.Value == null && y.Value == null)
+						return 0;
+					else
+						return x.Value.CalculatedStageTime.CompareTo(y.Value.CalculatedStageTime);
+				});
+
+				foreach (KeyValuePair<string, DriverTime> driverTimeKvp in sortedStageData)
+				{
+					var driverName = driverTimeKvp.Key;
+					var driverTime = driverTimeKvp.Value;
+
+					if (driverTime != null)
+					{
+						var formatString = @"mm\:ss\.ff";
+						var line = driverTime.CaclulatedStagePosition + "," +
+								   driverTime.Driver + "," +
+								   driverTime.Vehicle + "," +
+								   driverTime.CalculatedStageTime.ToString(formatString) + "," +
+								   driverTime.CalculatedStageDiffFirst.ToString(formatString) + "," +
+								   driverTime.CalculatedStageDiffPrevious.ToString(formatString);
+
+						outputSB.AppendLine(line);
+					}
+					else
+					{
+						outputSB.AppendLine(",," + driverName + ",DNF");
+					}
+				}
+
+				outputSB.AppendLine("");
+				stageCount++;
+			}
+
+			textBox_resultsInput.Text = outputSB.ToString();
+		}
+    }
+}
---
+++ DRTimeCruncher/DRTimeCruncher.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace DRTimeCruncher
+{
+    /// <summary>
+    /// Holds all stage times
+    /// </summary>
+    public class Rally : IEnumerable
+    {
+		public int StageCount { get { return stages.Count; } }
+
+        private List<Stage> stages = new List<Stage>();
+
+        /// <summary>
+        /// Adds a stage to the rally's stage collection
+		/// Enumerating returns Stages
+        /// </summary>
+        /// <returns>true</returns>
+        public bool AddStage(Stage stage)
+        {
+            stages.Add(stage);
+            return true;
+        }
+
+        public bool CalculateTimes()
+        {
+			// create lookup tables for comparing times
+            var previousStage = new Dictionary<string, DriverTime>();
+            var currentStage = new Dictionary<string, DriverTime>();
+
+			previousStage = stages[0].DriverTimes;
+
+			CalculateDeltas(stages[0], true);
+
+			// for each stage after SS1
+			for (int i = 1; i < stages.Count; i++)
+			{
+				currentStage = stages[i].DriverTimes;
+
+				// for each driver on the previous stage
+				foreach (KeyValuePair<string,DriverTime> previousDriverTimeKvp in previousStage)
+				{
+					// get the current time and compute the stage time and position change
+					DriverTime currentDriverTime;
+					if (true == currentStage.TryGetValue(previousDriverTimeKvp.Key, out currentDriverTime))
+					{
+						if (currentDriverTime != null)
+						{
+							currentDriverTime.CalculatedStageTime = currentDriverTime.CalculatedOverallTime - previousDriverTimeKvp.Value.CalculatedOverallTime;
+							currentDriverTime.CalculatedPositionChange = previousDriverTimeKvp.Value.OverallPosition - currentDriverTime.OverallPosition;
+						}
+					}
+					else
+					{
+						currentStage.Add(previousDriverTimeKvp.Key, null); // track DNFs
+					}
+				}
+
+				previousStage = currentStage;
+
+				CalculateDeltas(stages[i], false);
+			}
+
+            return true;
+        }
+
+		private void CalculateDeltas(Stage currentStage, bool isFirstStage)
+		{
+			// order by overall time and calculate differences
+			TimeSpan fastestOverallTime;
+			TimeSpan previousOverallTime;
+			TimeSpan fastestStageTime;
+			TimeSpan previousStageTime;
+			foreach (DriverTime driverTime in currentStage.DriverTimes.Values.Where(x => x != null).OrderBy(x => x.CalculatedOverallTime))
+			{
+				if (driverTime == null)
+					continue;
+
+				if (driverTime.OverallPosition == 1)
+				{
+					fastestOverallTime = driverTime.CalculatedOverallTime;
+					previousOverallTime = driverTime.CalculatedOverallTime;
+
+					if (isFirstStage == true)
+					{
+						driverTime.CaclulatedStagePosition = driverTime.OverallPosition;
+						driverTime.CalculatedStageTime = driverTime.CalculatedOverallTime;
+					}
+
+					continue;
+				}
+
+				driverTime.CalculatedOverallDiffFirst = driverTime.CalculatedOverallTime - fastestOverallTime;
+				driverTime.CalculatedOverallDiffPrevious = driverTime.CalculatedOverallTime - previousOverallTime;
+
+				if (isFirstStage == true)
+				{
+					driverTime.CaclulatedStagePosition = driverTime.OverallPosition;
+					driverTime.CalculatedStageTime = driverTime.CalculatedOverallTime;
+					driverTime.CalculatedStageDiffFirst = driverTime.CalculatedOverallDiffFirst;
+					driverTime.CalculatedStageDiffPrevious = driverTime.CalculatedOverallDiffPrevious;
+				}
+
+				previousOverallTime = driverTime.CalculatedOverallTime;
+			}
+
+			// order by stage time and calculate differences
+			if (isFirstStage == false)
+			{
+				int stagePosition = 1;
+				foreach (DriverTime driverTime in currentStage.DriverTimes.Values.Where(x => x != null).OrderBy(x => x.CalculatedStageTime))
+				{
+					if (driverTime == null)
+					{
+						stagePosition++;
+						continue;
+					}
+					if (stagePosition == 1)
+					{
+						fastestStageTime = driverTime.CalculatedStageTime;
+						previousStageTime = driverTime.CalculatedStageTime;
+						driverTime.CaclulatedStagePosition = stagePosition;
+						stagePosition++;
+						continue;
+					}
+
+					driverTime.CalculatedStageDiffFirst = driverTime.CalculatedStageTime - fastestStageTime;
+					driverTime.CalculatedStageDiffPrevious = driverTime.CalculatedStageTime - previousStageTime;
+					driverTime.CaclulatedStagePosition = stagePosition;
+					previousStageTime = driverTime.CalculatedStageTime;
+					stagePosition++;
+				}
+			}
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return ((IEnumerable)stages).GetEnumerator();
+		}
+	}
+
+    /// <summary>
+    /// Holds all driver times for a single stage
+	/// Enumeration returns DriverTimes
+    /// </summary>
+    public class Stage : IEnumerable
+    {
+		public Dictionary<string, DriverTime> DriverTimes { get; private set; }
+
+        /// <summary>
+        /// Adds a driver's stage results to the stage's collection
+        /// </summary>
+        /// <returns>true</returns>
+        public bool AddDriver(DriverTime driverTime)
+        {
+			DriverTimes.Add(driverTime.Driver, driverTime);
+            return true;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+			return ((IEnumerable)DriverTimes).GetEnumerator();
+        }
+
+		public Stage()
+		{
+			DriverTimes = new Dictionary<string, DriverTime>();
+		}
+    }
+
+    /// <summary>
+    /// Holds the data for a single driver on a single stage
+    /// </summary>
+    public class DriverTime
+    {
+        // raw data supplied by parser
+        public int OverallPosition { get; private set; }
+        public string Tags { get; private set; }
+        public string Driver { get; private set; }
+        public string Vehicle { get; private set; }
+        public string OverallTime { get; private set; }
+        public string OverallDiffFirst { get; private set; }
+
+        // calculated overall data
+        public TimeSpan CalculatedOverallTime { get; internal set; }
+		public TimeSpan CalculatedOverallDiffPrevious { get; internal set; }
+		public TimeSpan CalculatedOverallDiffFirst { get; internal set; }
+		public int CalculatedPositionChange { get; internal set; }
+
+        // calculated stage data
+		public int CaclulatedStagePosition { get; internal set; }
+		public TimeSpan CalculatedStageTime { get; internal set; }
+		public TimeSpan CalculatedStageDiffPrevious { get; internal set; }
+		public TimeSpan CalculatedStageDiffFirst { get; internal set; }
+
+        /// <summary>
+        /// Creates a new DriverData object that represents a single driver's time on a single stage.
+        /// Data is the 'raw' string data taken from the results
+        /// Tags are optional (not all drivers will have tags)
+        /// </summary>
+        public DriverTime(int overallPosition, string driver, string vehicle, string overallTime, string overallDiffFirst, string tags = null)
+        {
+            OverallPosition = overallPosition;
+            Tags = tags;
+            Driver = driver;
+            Vehicle = vehicle;
+            OverallTime = overallTime;
+            OverallDiffFirst = overallDiffFirst;
+
+			CalculatedOverallTime = TimeSpan.ParseExact(OverallTime, @"mm\:ss\.ff", CultureInfo.InvariantCulture);
+        }
+    }
+}
