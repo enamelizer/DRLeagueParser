588373e484f53ca7e6c23f413b64c89ee84a8865
aarondemarre <ademarre@gmail.com>
2016-05-03 11:02:14 -0700

    UI improvements
    Added combo box for output types
    Stubbed out chart output type
    Added vertical scrollbar
    Added copy to clipboard button
    Code formatting fixes

--- DRLPTest/DRParserTest.xaml.cs
+++ DRLPTest/DRParserTest.xaml.cs
@@ -20,7 +20,33 @@ namespace DRLPTest
             InitializeComponent();
 
             rallyData = new Rally();
+            DataContext = this;
         }
+
+        // enum for the type of output to print
+        public enum PrintType
+        {
+            Stages,
+            Overall,
+            Chart
+        }
+
+        // binds to the print type combobox
+        private Dictionary<PrintType, string> printTypeDict = new Dictionary<PrintType, string>
+        {
+            { PrintType.Stages, "Stages" },
+            { PrintType.Overall, "Overall" },
+            { PrintType.Chart, "Chart" }
+        };
+
+        public Dictionary<PrintType, string> PrintTypeDict
+        {
+            get { return printTypeDict; }
+        }
+
+        // holds the user selected print type
+        public PrintType SelectedPrintType { get; set; }
+
 
         private void button_parseStage_Click(object sender, RoutedEventArgs e)
         {
@@ -70,136 +96,153 @@ namespace DRLPTest
             rallyData.AddStage(stageData);
 
             textBox_resultsInput.Clear();
-			label_statusMessage.Content = String.Format("SS{0} Has been parsed.", rallyData.StageCount);
+            label_statusMessage.Content = String.Format("SS{0} Has been parsed.", rallyData.StageCount);
             label_statusMessage.Foreground = Brushes.Green;
         }
 
-		private void button_crunchNumbers_Click(object sender, RoutedEventArgs e)
-		{
-			rallyData.CalculateTimes();
+        private void button_crunchNumbers_Click(object sender, RoutedEventArgs e)
+        {
+            rallyData.CalculateTimes();
             label_statusMessage.Content = "Numbers Crunched";
             label_statusMessage.Foreground = Brushes.Green;
         }
 
-		private void button_clearAllData_Click(object sender, RoutedEventArgs e)
-		{
-			rallyData = new Rally();
+        private void button_clearAllData_Click(object sender, RoutedEventArgs e)
+        {
+            rallyData = new Rally();
             label_statusMessage.Content = "Data Cleared";
             label_statusMessage.Foreground = Brushes.Green;
             textBox_resultsInput.Clear();
-		}
+        }
 
-		private void button_printOverallTimes_Click(object sender, RoutedEventArgs e)
-		{
-			var outputSB = new StringBuilder();
+        private void printOverallTimes()
+        {
+            var outputSB = new StringBuilder();
 
-			int stageCount = 1;
-			foreach (Stage stage in rallyData)
-			{
-				outputSB.AppendLine("SS" + stageCount);
-				outputSB.AppendLine("Overall");
-				outputSB.AppendLine("Pos, Pos Chng, Name, Vehicle, Time, Diff 1st, Diff Prev");
+            int stageCount = 1;
+            foreach (Stage stage in rallyData)
+            {
+                outputSB.AppendLine("SS" + stageCount);
+                outputSB.AppendLine("Overall");
+                outputSB.AppendLine("Pos, Pos Chng, Name, Vehicle, Time, Diff 1st, Diff Prev");
 
-				List<KeyValuePair<string, DriverTime>> sortedStageData = stage.DriverTimes.ToList();
-				sortedStageData.Sort((x, y) =>
-				{
-					if (x.Value != null && y.Value == null)
-						return -1;
-					else if (x.Value == null && y.Value != null)
-						return 1;
-					else if (x.Value == null && y.Value == null)
-						return 0;
-					else
-						return x.Value.CalculatedOverallTime.CompareTo(y.Value.CalculatedOverallTime);
-				});
+                List<KeyValuePair<string, DriverTime>> sortedStageData = stage.DriverTimes.ToList();
+                sortedStageData.Sort((x, y) =>
+                {
+                    if (x.Value != null && y.Value == null)
+                        return -1;
+                    else if (x.Value == null && y.Value != null)
+                        return 1;
+                    else if (x.Value == null && y.Value == null)
+                        return 0;
+                    else
+                        return x.Value.CalculatedOverallTime.CompareTo(y.Value.CalculatedOverallTime);
+                });
 
-				foreach (KeyValuePair<string, DriverTime> driverTimeKvp in sortedStageData)
-				{
-					var driverName = driverTimeKvp.Key;
-					var driverTime = driverTimeKvp.Value;
+                foreach (KeyValuePair<string, DriverTime> driverTimeKvp in sortedStageData)
+                {
+                    var driverName = driverTimeKvp.Key;
+                    var driverTime = driverTimeKvp.Value;
 
-					if (driverTime != null)
-					{
-						var formatString = @"mm\:ss\.ff";
-						var line = driverTime.OverallPosition + "," +
-								   driverTime.CalculatedPositionChange + "," +
-								   driverTime.Driver + "," +
-								   driverTime.Vehicle + "," +
-								   driverTime.CalculatedOverallTime.ToString(formatString) + "," +
-								   driverTime.CalculatedOverallDiffFirst.ToString(formatString) + "," +
-								   driverTime.CalculatedOverallDiffPrevious.ToString(formatString);
+                    if (driverTime != null)
+                    {
+                        var formatString = @"mm\:ss\.ff";
+                        var line = driverTime.OverallPosition + "," +
+                                   driverTime.CalculatedPositionChange + "," +
+                                   driverTime.Driver + "," +
+                                   driverTime.Vehicle + "," +
+                                   driverTime.CalculatedOverallTime.ToString(formatString) + "," +
+                                   driverTime.CalculatedOverallDiffFirst.ToString(formatString) + "," +
+                                   driverTime.CalculatedOverallDiffPrevious.ToString(formatString);
 
-						outputSB.AppendLine(line);
-					}
-					else
-					{
-						outputSB.AppendLine(",," + driverName + ",,DNF");
-					}
-				}
+                        outputSB.AppendLine(line);
+                    }
+                    else
+                    {
+                        outputSB.AppendLine(",," + driverName + ",,DNF");
+                    }
+                }
 
-				outputSB.AppendLine("");
-				stageCount++;
-			}
+                outputSB.AppendLine("");
+                stageCount++;
+            }
 
             label_statusMessage.Content = "Displaying Overall Times";
             label_statusMessage.Foreground = Brushes.Green;
             textBox_resultsInput.Text = outputSB.ToString();
-		}
+        }
 
-		private void button_printStageTimes_Click(object sender, RoutedEventArgs e)
-		{
-			var outputSB = new StringBuilder();
+        private void printStageTimes()
+        {
+            var outputSB = new StringBuilder();
 
-			int stageCount = 1;
+            int stageCount = 1;
 
-			foreach (Stage stage in rallyData)
-			{
-				outputSB.AppendLine("SS" + stageCount);
-				outputSB.AppendLine("Stage");
-				outputSB.AppendLine("Pos, Name, Vehicle, Time, Diff 1st, Diff Prev");
+            foreach (Stage stage in rallyData)
+            {
+                outputSB.AppendLine("SS" + stageCount);
+                outputSB.AppendLine("Stage");
+                outputSB.AppendLine("Pos, Name, Vehicle, Time, Diff 1st, Diff Prev");
 
-				List<KeyValuePair<string, DriverTime>> sortedStageData = stage.DriverTimes.ToList();
-				sortedStageData.Sort((x, y) =>
-				{
-					if (x.Value != null && y.Value == null)
-						return -1;
-					else if (x.Value == null && y.Value != null)
-						return 1;
-					else if (x.Value == null && y.Value == null)
-						return 0;
-					else
-						return x.Value.CalculatedStageTime.CompareTo(y.Value.CalculatedStageTime);
-				});
+                List<KeyValuePair<string, DriverTime>> sortedStageData = stage.DriverTimes.ToList();
+                sortedStageData.Sort((x, y) =>
+                {
+                    if (x.Value != null && y.Value == null)
+                        return -1;
+                    else if (x.Value == null && y.Value != null)
+                        return 1;
+                    else if (x.Value == null && y.Value == null)
+                        return 0;
+                    else
+                        return x.Value.CalculatedStageTime.CompareTo(y.Value.CalculatedStageTime);
+                });
 
-				foreach (KeyValuePair<string, DriverTime> driverTimeKvp in sortedStageData)
-				{
-					var driverName = driverTimeKvp.Key;
-					var driverTime = driverTimeKvp.Value;
+                foreach (KeyValuePair<string, DriverTime> driverTimeKvp in sortedStageData)
+                {
+                    var driverName = driverTimeKvp.Key;
+                    var driverTime = driverTimeKvp.Value;
 
-					if (driverTime != null)
-					{
-						var formatString = @"mm\:ss\.ff";
-						var line = driverTime.CaclulatedStagePosition + "," +
-								   driverTime.Driver + "," +
-								   driverTime.Vehicle + "," +
-								   driverTime.CalculatedStageTime.ToString(formatString) + "," +
-								   driverTime.CalculatedStageDiffFirst.ToString(formatString) + "," +
-								   driverTime.CalculatedStageDiffPrevious.ToString(formatString);
+                    if (driverTime != null)
+                    {
+                        var formatString = @"mm\:ss\.ff";
+                        var line = driverTime.CaclulatedStagePosition + "," +
+                                   driverTime.Driver + "," +
+                                   driverTime.Vehicle + "," +
+                                   driverTime.CalculatedStageTime.ToString(formatString) + "," +
+                                   driverTime.CalculatedStageDiffFirst.ToString(formatString) + "," +
+                                   driverTime.CalculatedStageDiffPrevious.ToString(formatString);
 
-						outputSB.AppendLine(line);
-					}
-					else
-					{
-						outputSB.AppendLine("," + driverName + ",,DNF");
-					}
-				}
+                        outputSB.AppendLine(line);
+                    }
+                    else
+                    {
+                        outputSB.AppendLine("," + driverName + ",,DNF");
+                    }
+                }
 
-				outputSB.AppendLine("");
-				stageCount++;
-			}
+                outputSB.AppendLine("");
+                stageCount++;
+            }
             label_statusMessage.Content = "Displaying Stage Times";
             label_statusMessage.Foreground = Brushes.Green;
             textBox_resultsInput.Text = outputSB.ToString();
-		}
+        }
+
+        private void button_print_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedPrintType == PrintType.Stages)
+                printStageTimes();
+            else if (SelectedPrintType == PrintType.Overall)
+                printOverallTimes();
+            else if (SelectedPrintType == PrintType.Chart)
+                textBox_resultsInput.Text = "TODO: chart output";
+        }
+
+        private void button_copy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(textBox_resultsInput.Text);
+            label_statusMessage.Content = "Data Copied to Clipboard";
+            label_statusMessage.Foreground = Brushes.Green;
+        }
     }
 }
